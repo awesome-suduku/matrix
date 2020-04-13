8834028e3dfb12c7d0ecd44aaac510447f51a86e
lax <lax263@zrtg.com>
2020-04-13 10:38:20 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
 /*
- * @Description:
+ * @Description: vue配置文件
  * @Version: 1.0.0
  * @Autor: lax
  * @Date: 2020-04-01 12:54:53
  * @LastEditors: lax
- * @LastEditTime: 2020-04-01 12:54:55
+ * @LastEditTime: 2020-04-13 10:28:56
  */
 module.exports = {
+  /* vue 断点 */
   configureWebpack: {
     devtool: "source-map"
-  }
+  },
+  /* 设置buidl的引用文件路径 */
+  publicPath: "./"
 };
