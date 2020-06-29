d422c5e2a33d5c1d83d5089c42df7ed3dad66e0d
lax <756993539@qq.com>
2020-06-29 20:06:50 +0800

    reflash gitignore

---
+++ src/views/Index.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Description: 
+ * @Version: 1.0.0
+ * @Autor: lax
+ * @Date: 2020-04-07 14:34:37
+ * @LastEditors: lax
+ * @LastEditTime: 2020-06-28 09:58:56
+ -->
+<template>
+  <tao-container>
+    <tao-music v-show="display" />
+    <tao-loading v-if="!is_ready" :color="'pink'"></tao-loading>
+    <tao-page v-show="false"></tao-page>
+  </tao-container>
+</template>
+
+<script>
+import wxHandler from "vue-wx-tools";
+export default {
+  name: "Index",
+  data: function() {
+    return {
+      is_ready: false,
+      display: false
+    };
+  },
+  created: function() {
+    const self = this;
+    new wxHandler({
+      debug: true,
+      over: function() {
+        self.is_ready = true;
+        self.display = true;
+      }
+    }).execute();
+  }
+};
+</script>
